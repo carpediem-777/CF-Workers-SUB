3e1dbd75064211c9223db89958ed4a173807a8b4
Dylan <carpediem.dwk@gmail.com>
2024-06-22 12:47:20 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -12,7 +12,7 @@ let SUBUpdateTime = 6; //自定义订阅更新时间，单位小时
 
 //节点链接 + 订阅链接
 let MainData = `
-vless://0e354051-bcec-43c9-fb55-aa8120229fe5@23.106.131.167:1000?type=tcp&security=reality&sni=authorize.videvo.net&pbk=cDaDzPr3PlS3NM8lreHZbdo-Mhqz8vMBzMSkHXhGIUA&flow=xtls-rprx-vision&fp=chrome#JP-CN2-GIA|Dylan.love@xray.com
+vless://b7a392e2-4ef0-4496-90bc-1c37bb234904@cf.090227.xyz:443?encryption=none&security=tls&sni=edgetunnel-2z2.pages.dev&fp=random&type=ws&host=edgetunnel-2z2.pages.dev&path=%2F%3Fed%3D2048#%E5%8A%A0%E5%85%A5%E6%88%91%E7%9A%84%E9%A2%91%E9%81%93t.me%2FCMLiussss%E8%A7%A3%E9%94%81%E6%9B%B4%E5%A4%9A%E4%BC%98%E9%80%89%E8%8A%82%E7%82%B9
 `
 
 let urls = [];
